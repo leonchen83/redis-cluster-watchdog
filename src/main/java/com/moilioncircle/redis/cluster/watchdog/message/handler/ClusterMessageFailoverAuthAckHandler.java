51e32573f25b1d9e7ca5dbe6cd5ff0a6731d2e57
leon <chen.bao.yi@qq.com>
2017-07-24 10:34:55 +0800

    Init

--- src/main/java/com/moilioncircle/redis/cluster/watchdog/message/handler/ClusterMessageFailoverAuthAckHandler.java
+++ src/main/java/com/moilioncircle/redis/cluster/watchdog/message/handler/ClusterMessageFailoverAuthAckHandler.java
@@ -24,7 +24,7 @@ public class ClusterMessageFailoverAuthAckHandler extends AbstractClusterMessage
 
         if (sender == null) return true;
         if (nodeIsMaster(sender) && sender.assignedSlots > 0 && hdr.currentEpoch >= server.cluster.failoverAuthEpoch) {
-            server.cluster.failoverAuthEpoch++;
+            server.cluster.failoverAuthCount++;
             managers.failovers.clusterHandleSlaveFailover();
         }
         return true;
