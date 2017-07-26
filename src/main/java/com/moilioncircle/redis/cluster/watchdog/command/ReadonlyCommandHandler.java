959e484b7c95653c6a38169927bb34d16f6a328d
leon <chen.bao.yi@qq.com>
2017-07-26 14:13:52 +0800

    support restore command

---
+++ src/main/java/com/moilioncircle/redis/cluster/watchdog/command/ReadonlyCommandHandler.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016 leon chen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.moilioncircle.redis.cluster.watchdog.command;
+
+import com.moilioncircle.redis.cluster.watchdog.manager.ClusterManagers;
+import com.moilioncircle.redis.cluster.watchdog.util.net.transport.Transport;
+
+/**
+ * @author Leon Chen
+ * @since 2.1.0
+ */
+public class ReadonlyCommandHandler extends AbstractCommandHandler {
+
+    public ReadonlyCommandHandler(ClusterManagers managers) {
+        super(managers);
+    }
+
+    @Override
+    public void handle(Transport<Object> t, String[] message, byte[][] rawMessage) {
+        if (message.length != 1) {
+            replyError(t, "wrong number of arguments for 'readonly' command");
+        }
+
+        reply(t, "OK");
+    }
+}
